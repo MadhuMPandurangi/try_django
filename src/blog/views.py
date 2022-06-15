197e31f32a73b50b8c35dee9a657ac45c516a400
MadhuMPandurangi <madhumpandurangi@gmail.com>
2022-06-15 16:24:25 +0530

    Crud view outline updated

--- src/blog/views.py
+++ src/blog/views.py
@@ -27,15 +27,40 @@ def blog_post_detail_page(request, slug):
 
 # POST -> Create / Update / Delete
 
-# def blog_post_list_view(request):
-#     return
+def blog_post_list_view(request):
+    # list out objects
+    # this could also be search view
+    template_name = 'blog_post_list.html'
+    context = {'object_list': []}
+    return render(request, template_name, context)
 
-# def blog_post_create_view(request):
-#     return
-# def blog_post_retrieve_view(request):
-#     return
-# def blog_post_update_view(request):
-#     return
-# def blog_post_delete_view(request):
-#     return
+
+def blog_post_create_view(request):
+    # create objects
+    # ? use a form
+    template_name = 'blog_post_create.html'
+    context = {'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_detail_view(request, slug):
+    # 1 object -> detail view
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_detail.html'
+    context = {"object": obj}
+    return render(request, template_name, context)
+
+
+def blog_post_update_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_update.html'
+    context = {"object": obj, 'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_delete_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_delete.html'
+    context = {"object": obj}
+    return render(request, template_name, context)
        
